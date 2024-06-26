233b6d96f23913f19c5c852a9cd49cfec842c91a
48111030 <48111030@est.ort.edu.ar>
2024-06-26 16:00:24 -0300

    RandomPlacement, FoodInstantiator
    establecemos los prefabs de manera aleatoria en el plano de forma consecutiva

---
+++ Assets/Scenes/scripts/FoodInstantiator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Agregar una variable que establezca la cantidad de veces que se instanciará 
+//un alimento. Cuando se instancie esa cantidad de alimentos, no deben instanciarse más
+public class FoodInstantiator : MonoBehaviour
+{
+    public GameObject[] alimentos;
+    public Transform clonePoint;
+    public float interval;
+    public RandomPlacement posicionAleatoriaDeClonePoint;
+    public int cloneCount = 0;
+    public int maxClones;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(CloneFood), 0, interval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void CloneFood()
+    {
+        if (cloneCount < maxClones)
+        {
+            cloneCount++;
+            posicionAleatoriaDeClonePoint.SetRandomPosition();
+            GameObject prefab = alimentos[Random.Range(0, alimentos.Length)];
+            Instantiate(prefab, clonePoint.position, clonePoint.rotation);
+        }
+
+    }
+}
+
