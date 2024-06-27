c66dcaaaf218548bf001d7aa339af8585ddac95b
Joaquin Blanco <48386218@est.ort.edu.ar>
2024-06-27 08:22:16 -0300

    Added basic functions, and finished UI

---
+++ Assets/PanelController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelController : MonoBehaviour
+{
+    public GameObject panelBase;
+    public GameObject panelInputVacio;
+    public GameObject panelRespuestaCorrecta;
+    public GameObject panelRespuestaIncorrecta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void HideInputVacioPanel()
+    {
+        panelInputVacio.SetActive(false);
+    }
+}
