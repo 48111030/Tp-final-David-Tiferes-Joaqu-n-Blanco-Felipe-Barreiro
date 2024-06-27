793dd982122b5a0c5e0c6d4d0a5c62bfb05f610c
Joaquin Blanco <48386218@est.ort.edu.ar>
2024-06-27 09:00:15 -0300

    Added UI to main Scene + major functions
    Falta arreglar bugs al enviar la respuesta

--- Assets/PanelController.cs
+++ Assets/PanelController.cs
@@ -10,6 +10,8 @@ public class PanelController : MonoBehaviour
     public GameObject panelRespuestaCorrecta;
     public GameObject panelRespuestaIncorrecta;
 
+    public FoodInstantiator script;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,5 +27,39 @@ public class PanelController : MonoBehaviour
     private void HideInputVacioPanel()
     {
         panelInputVacio.SetActive(false);
+        panelBase.SetActive(true);
+    }
+
+    private void HideRespuestaIncorrectaPanel()
+    {
+        panelRespuestaIncorrecta.SetActive(false);
+        panelBase.SetActive(true);
+    }
+
+    private void CheckAnswer()
+    {
+        /*
+        if (panelBase.input_Respuesta.text == script.maxClones)
+        {
+            panelRespuestaCorrecta.SetActive(true);
+            panelBase.SetActive(false);
+        }
+        else if (panelBase.input_Respuesta.text == "")
+        {
+            panelBase.SetActive(false);
+            panelInputVacio.SetActive(true);
+        } 
+        else
+        {
+            panelRespuestaIncorrecta.SetActive(true);
+            panelBase.SetActive(false);
+        }
+
+       */
+    }
+
+    private void StartGame()
+    {
+
     }
 }
